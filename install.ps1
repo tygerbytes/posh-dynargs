52045de02388fca34e30862cca9262c784c36196
Ty Walls <tygerbytes@tygertec.com>
2019-01-13 21:25:54 -0500

    install.ps1: Source Utils.ps1 instead of importing posh-dynargs.psd1
    The Chocolatey package verification tests use PowerShell 4.0, but
    the posh-dynargs module requires 5.0 or better. The package has a
    dependency on powershell (5.1), but it requires a reboot, so it's not
    ready in time to successfully import the module.

--- install.ps1
+++ install.ps1
@@ -2,6 +2,6 @@ param([switch]$WhatIf = $false, [switch]$Force = $false, [switch]$Verbose = $fal
 
 $installDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-Import-Module $installDir\src\posh-dynargs.psd1
+. $installDir\src\Utils.ps1
 
 Add-PoshDynargsToProfile -WhatIf:$WhatIf -Force:$Force -Verbose:$Verbose
